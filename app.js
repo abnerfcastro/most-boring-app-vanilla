61575f7e8fc90ea8eb946fb1feefbd0d2919a100
Abner Castro <abnerfcastro@gmail.com>
2021-08-04 18:33:34 -0300

    Implement step-4 of the presentation

--- app.js
+++ app.js
@@ -2,3 +2,21 @@
 let h1Elem = document.getElementById('greeting-message');
 
 h1Elem.textContent = 'Hello, SA Team!';
+
+let value = 0;
+
+function updateValueText() {
+    document.getElementById('value-wrapper').textContent = `${value}`;
+}
+
+let incrementBtn = document.getElementById('increment-btn');
+
+incrementBtn.addEventListener('click', () => {
+    value += 1;
+    updateValueText();
+});
+
+function decrementValue() {
+    value -= 1;
+    updateValueText();
+};
