fcfc103c21b0f9aafb8d1c0c792ff5f1bf292849
Abner Castro <abnerfcastro@gmail.com>
2021-08-04 19:09:08 -0300

    Implement step-5 of the presentation

--- app.js
+++ app.js
@@ -20,3 +20,54 @@ function decrementValue() {
     value -= 1;
     updateValueText();
 };
+
+// ----
+
+let fetchStarWarsCharactersBtn = document.getElementById('fetch-star-wars-characters');
+
+function setLoading() {
+    const wrapperDiv = document.getElementById('star-wars-characters-wrapper');
+    wrapperDiv.innerHTML = '<span>Loading...</span>';
+}
+
+function renderStarWarsCharactersList(characters) {
+    if (Array.isArray(characters) === false) return;
+    const listElem = document.createElement('ul');
+    for (const { name } of characters) {
+        const listItemElem = document.createElement('li');
+        listItemElem.textContent = name;
+        listElem.appendChild(listItemElem);
+    }
+    const wrapperDiv = document.getElementById('star-wars-characters-wrapper');
+    wrapperDiv.innerHTML = '';
+    wrapperDiv.appendChild(listElem);
+}
+
+function clearCharactersList() {
+    const wrapperDiv = document.getElementById('star-wars-characters-wrapper');
+    while (wrapperDiv.firstChild) {
+        wrapperDiv.removeChild(wrapperDiv.lastChild);
+    }
+}
+
+fetchStarWarsCharactersBtn.addEventListener('click', async () => {
+    try {
+        let data = { next: 'https://swapi.dev/api/people', results: [] }
+        let starWarsCharacters = [];
+        setLoading();
+
+        while (data.next) {
+            const response = await fetch(data.next);
+            if (response.ok) {
+                data = await response.json();
+                starWarsCharacters = [...starWarsCharacters, ...data.results]
+            } else {
+                alert(`Unable to fetch from SWAPI: ${data.next}`);
+                break;
+            }
+        }       
+        renderStarWarsCharactersList(starWarsCharacters);
+    } catch (error) {
+       console.error(error);
+    }
+});
